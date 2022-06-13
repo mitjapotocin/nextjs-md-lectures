456ca950fdfe59d2bbc03578e01e247f2da051d1
mitjapotocin <mitja.potocin@gmail.com>
2022-06-13 08:25:03 +0200

    Chapters cleanup

--- pages/chapters/[slug].js
+++ pages/chapters/[slug].js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import md from 'markdown-it';
-import { isAllowedPaths } from '../../utils/helpers';
+import { isAllowedPaths, parseMd } from '../../utils/helpers';
 import { useState } from 'react';
 
 export async function getStaticPaths() {
@@ -18,9 +18,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
   const fileName = fs.readFileSync(`posts/chapters/${slug}/index.md`, 'utf-8');
   const { data: frontmatter, content } = matter(fileName);
-  
-  let subPages = [];
-  
+
   return {
     props: {
       frontmatter,
@@ -30,21 +28,17 @@ export async function getStaticProps({ params: { slug } }) {
 }
 
 export default function PostPage({ frontmatter, content }) {
-  const [value, setValue] = useState("**Hello world!!!**");
-  // function handleEditorChange({ html, text }) {
-  //   console.log('handleEditorChange', html, text);
-  // }
   return (
     <div className='prose mx-auto'>
-      <h1>{frontmatter.title}</h1>
+      <div className='flex-container'>
+      <div  className='right-column'>
+      
+          <h1>{frontmatter.title}</h1>
 
-      <textarea
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      {/* <MdEditor style={{ height: '500px' }} renderHTML={text => md().render(text)} onChange={handleEditorChange} /> */}
-      <div dangerouslySetInnerHTML={{ __html: md({ html: true }).render(value) }} />
+          <div dangerouslySetInnerHTML={{ __html: md({ html: true }).render(parseMd(content)) }} />
 
+      </div>
+      </div>
     </div>
   );
 }
