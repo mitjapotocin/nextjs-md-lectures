0a797535af406a388d4f438ea0506a819ef64b9f
mitjapotocin <mitja.potocin@gmail.com>
2022-06-19 22:50:17 +0200

    Assets prefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  // basePath: '',
+  // assetPrefix: '/',
   images: {
     loader: 'custom',
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
